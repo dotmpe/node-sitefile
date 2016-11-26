53ba07e93e550f5f1c7e2057d8c0997c90deb49b
B. van Berkum <dev@dotmpe.com>
2016-11-26 04:29:33 +0100

    Webpack seems to be working OK

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var path = require('path');
 var fs = require('fs');
 var header = require('gulp-header');
 var chmod = require('gulp-chmod');
+var rename = require('gulp-rename');
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
@@ -18,8 +19,8 @@ var config = {
   entry: [ './bin/sitefile.coffee' ],
   target: 'node',
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'sitefile',
+    path: path.join(__dirname, 'dist'),
+    filename: 'sitefile.js',
     libraryTarget: "commonjs2",
     library: "sitefile_cli"
   },
@@ -27,23 +28,24 @@ var config = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
 			{ test: /\.coffee$/, loader: "coffee" },
-			{ test: /\.json$/, loader: "json" }
+			{ test: /\.json$/, loader: "json" },
+			{ test: /\.pug$/, loader: "pug" }
     ]
   },
   externals: nodeModules,
   plugins: [
-    new webpack.IgnorePlugin(/markdown|pug|stylus|pm2|pmx|knex|bookshelfapi/),
+    new webpack.IgnorePlugin(/^(markdown|pug|stylus|pm2|pmx|knex)$/),
     new webpack.IgnorePlugin(/\.(css|less)$/),
     new webpack.BannerPlugin('require("source-map-support").install();',
-                             { raw: true, entryOnly: false })
+                             { raw: true, entryOnly: false }),
   ],
 	resolve: {
 		extensions: [
-			"", ".coffee", ".js", ".json"
+			'', '.coffee', '.js', '.json', '.pug'
 		]
 	},
   devtool: 'sourcemap',
-}
+};
 
 gulp.task('server-build', function(done) {
   webpack(config).run(function(err, stats) {
@@ -53,11 +55,11 @@ gulp.task('server-build', function(done) {
     else {
       console.log(stats.toString());
     }
-    gulp.src('build/sitefile')
+    gulp.src('dist/sitefile.js')
       .pipe(header('#!/usr/bin/env node\n'))
       .pipe(chmod(0755))
+      .pipe(rename('sitefile'))
       .pipe(gulp.dest('./dist'))
-
     done();
   });
 });
