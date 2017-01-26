42442b7f114bcb8999f7dc9c75009fc19a0fc57d
Travis <dev+travis@dotmpe.com>
2017-01-26 22:00:40 +0100

    Docker: entry.sh fix

--- docker/ubuntu-1704/entry.sh
+++ docker/ubuntu-1704/entry.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-test -n "$1" || set -- "node-sitefile" "$2" "$3"
-test -n "$2" || set -- "$1" "master" "$3"
-
-test -d "/src/$1" || {
-  test -n "$3" || {
-    echo "Missing repo for site '$1'"
-    exit 1
-  }
-}
-
-cd $1
-
-git checkout $2
-
-sitefile
-
+../entry.sh
