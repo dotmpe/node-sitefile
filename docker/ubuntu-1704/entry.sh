c1adc32b28292ec83afdcb57dbfffd7c5cb80cdd
Travis <dev+travis@dotmpe.com>
2017-01-26 22:16:04 +0100

    -

--- docker/ubuntu-1704/entry.sh
+++ docker/ubuntu-1704/entry.sh
@@ -1,1 +1,18 @@
-../entry.sh
+#!/bin/bash
+
+test -n "$1" || set -- "node-sitefile" "$2" "$3"
+test -n "$2" || set -- "$1" "master" "$3"
+
+test -d "/src/$1" || {
+  test -n "$3" || {
+    echo "Missing repo for site '$1'"
+    exit 1
+  }
+}
+
+cd /src/$1
+
+git checkout $2
+
+sitefile
+
