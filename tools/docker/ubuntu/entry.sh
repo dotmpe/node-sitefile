93f749442391d4af8f23724f2f1c1ca9b71e2729
Travis <dev+travis@dotmpe.com>
2017-09-24 20:08:28 +0200

    Fixed docker build

--- tools/docker/ubuntu/entry.sh
+++ tools/docker/ubuntu/entry.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-test -n "$1" || set -- "node-sitefile" "$2" "$3"
-test -n "$2" || set -- "$1" "master" "$3"
+test -n "$1" || set -- "github.com/bvberkum/node-sitefile" "$2" "$3"
 
 test -d "/src/$1" || {
   test -n "$3" || {
     echo "Missing repo for site '$1'"
     exit 1
   }
+  git clone $3 /src/$1
 }
 
 cd /src/$1
 
-git checkout $2
+test -z "$2" || git checkout $2
 
 test ! -e package.json || npm install
 
