b38e0f5b0e4b2ed4472f0ca28c54901d4cfd2800
Travis <dev+travis@dotmpe.com>
2017-09-24 20:35:49 +0200

    Updating r0.0.7 for Docker autobuild

---
+++ tools/docker/ubuntu/entry.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+test -n "$1" || set -- "github.com/bvberkum/node-sitefile" "$2" "$3"
+
+test -d "/src/$1" || {
+  test -n "$3" || {
+    echo "Missing repo for site '$1'"
+    exit 1
+  }
+  git clone $3 /src/$1
+}
+
+cd /src/$1
+
+test -z "$2" || git checkout $2
+
+test ! -e package.json || npm install
+
+sitefile
