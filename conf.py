5d465526a24f67f9516b8949c1a45b2282d8a174
B. van Berkum <dev@dotmpe.com>
2017-10-02 01:25:39 +0200

    RTD conf

--- conf.py
+++ conf.py
@@ -13,4 +13,6 @@ intersphinx_mapping = {
     'python': ('http://python.readthedocs.io/en/latest/', None),
     'django': ('http://django.readthedocs.io/en/1.8.x/', None),
     'sphinx': ('http://sphinx.readthedocs.io/en/latest/', None),
+    'script-mpe': ('http://script-mpe.readthedocs.io/en/latest/', None),
+    'node-sitefile': ('http://node-sitefile.readthedocs.io/en/latest/', None),
 }
