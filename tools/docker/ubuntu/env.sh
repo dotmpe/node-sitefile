0eb652e4bd0af06635a1e4989a60249c8cd6b139
B. van Berkum <dev@dotmpe.com>
2019-12-08 14:11:10 +0100

    Deps, build-tools.

--- tools/docker/ubuntu/env.sh
+++ tools/docker/ubuntu/env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e -o pipefail -o nounset
+
 true "${X_DCKR_CI_TIME:="` git show -s --format=%cI $GIT_SHA1`"}"
 true "${X_DCKR_AI_TIME:="` git show -s --format=%aI $GIT_SHA1`"}"
 
@@ -10,3 +12,17 @@ urlsafe_datetime()
 
   echo "$1" |tr ' ' 'T' |sed 's/+00:00/Z/g' |sed 's/+/%2B/g'
 }
+
+abort_build()
+{
+  e=$?
+  { cat <<EOM
+Error $e at $BASH_COMMAND :$BASH_LINENO
+EOM
+} >&2
+  curl -sSf -X POST \
+    "https://dotmpe.com/build.php?i=${BUILD_KEY}&c=3&e=${e}&BUILD_ID=${BUILD_CODE}&ABORT=$(urlsafe_datetime)"
+  return $e
+}
+
+trap abort_build EXIT
